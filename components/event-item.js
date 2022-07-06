a365faeabe5972f0156c540cf32d2791adbc7da9
VArox2x <hubr2000@gmail.com>
2022-07-07 01:19:27 +0200

    07.07 01:19

---
+++ components/event-item.js
@@ -0,0 +1,43 @@
+import classes from './event-item.module.css'
+import Button from "./ui/button";
+import DateIcon from "./icons/date-icon";
+import AddressIcon from "./icons/address-icon";
+import ArrowRightIcon from "./icons/arrow-right-icon";
+
+function EventItem (props){
+
+    const {title, image, date, location, id} = props;
+
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+    const formattedAddress = location.replace(', ', '\n')
+    const expoloreLink = `/events/${id}`
+
+    return (
+        <li className={classes.item}>
+        <img src={'/' + image} alt={title}/>
+        <div className={classes.content}>
+            <div><h2>{title}</h2></div>
+            <div className={classes.date}>
+                <DateIcon/>
+                <time>{humanReadableDate}</time>
+            </div>
+            <div className={classes.address}>
+                <AddressIcon/>
+                <address>{formattedAddress}</address>
+            </div>
+        </div>
+        <div className={classes.actions}>
+            <Button link={expoloreLink}>
+                <span>Explore  Event</span>
+                <span className={classes.icon}><ArrowRightIcon/></span>
+            </Button>
+        </div>
+    </li>
+    )
+}
+
+export default EventItem
--- pages/events/[eventid].js
+++
@@ -1,7 +0,0 @@
-export default function EventsIdPage() {
-    return (
-        <div>
-            <h1>Events Page Single Id Show selected event</h1>
-        </div>
-    )
-}
